878445482e30e14bd0a9819e1f5f666ff7aaed08
Wilson Neto <wilsonpsn@hotmail.com>
2019-09-12 22:31:47 -0300

    Agora tem JWT

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
-
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 
 // @route   POST api/users
@@ -18,12 +20,52 @@ router.post(
       min: 6
     })
   ],
-  (req, resp) => {
+  async (req, resp) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return resp.status(400).json({ errors: errors.array() })
     }
-    resp.send('passed')
+    const { name, email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (user) {
+        return resp.status(400).json({ msg: 'User already exists' })
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err
+          resp.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+      resp.status(500).send('Server error')
+    }
   }
 )
 
