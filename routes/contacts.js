16825d7bd2a20918bc0341c58ffca370bd389e3f
Wilson Neto <wilsonpsn@hotmail.com>
2019-10-07 21:23:30 -0300

    Contacts get working

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../middleware/auth.js')
+const User = require('../models/User')
+const Contact = require('../models/Contact')
+
 
 // @route   GET api/contacts
 // @desc    Get all user contacts
 // @access  Private
-router.get('/', (req, resp)=> {
-  resp.send('Get all contacts')
+router.get('/', [auth, [
+  check('name', 'Name is require').not().isEmpty()
+]], async (req, resp)=> {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 })
+    resp.json(contacts)
+  } catch (error) {
+    console.error(error.message)
+    resp.status(500).send('server error')
+  }
 })
 
 
 // @route   POST api/contacts
 // @desc    Add a contact
 // @access  Private
-router.post('/', (req, resp)=> {
+router.post('/', auth, (req, resp)=> {
   resp.send('Add a contact');
 })
 
