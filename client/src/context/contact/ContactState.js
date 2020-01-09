f94e1f196f89fd23d854565e753afc5ee421646a
Wilson <wilson.neto@nerdweb.com.br>
2020-01-09 13:23:58 -0300

    Added Dummy Data

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -3,3 +3,40 @@ import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 
+import { 
+  ADD_CONTACT, 
+  DELETE_CONTACT, 
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER 
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Jahnson',
+        email: 'jill@jill.jill',
+        phone: '111-1111-111',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Jall Jahnson',
+        email: 'jall@jall.jall',
+        phone: '222-2222-222',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Jeuu Jahnson',
+        email: 'jeuu@jeuu.jeuu',
+        phone: '333-3333-333',
+        type: 'personal'
+      }
+    ]
+  }
+}
