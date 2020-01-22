04cd34c26f00fcbb424ffad07ea1764ba49728b7
Wilson <wilson.neto@nerdweb.com.br>
2020-01-22 09:04:40 -0300

    Register reducer made

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,36 @@
+import {
+  REGISTER_SUCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case REGISTER_SUCESS:
+      console.log('Payload', action.payload);
+      localStorage.setItem('token', action.payload.token);
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticate: false,
+        loading: false,
+        user: null,
+        error: action.payload
+      };
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticate: true,
+        loading: false
+      };
+    default:
+      return state;
+  }
+};
